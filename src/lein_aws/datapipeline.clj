09df6a4adf4c6d3200fe082bff84d20529b0b279
Dan Herrera <whoahbot@gmail.com>
2013-07-21 15:47:16 -0700

    Add usage docs for functions in the datapipeline namespace.

--- src/lein_aws/datapipeline.clj
+++ src/lein_aws/datapipeline.clj
@@ -5,18 +5,21 @@
             [amazonica.aws.datapipeline :as aws]))
 
 (defn get-definition
+  "Get the pipeline definition for `pipeline-id`."
   [project [pipeline-id]]
   (let [config (:aws project)]
     (with-credential [(:access-key config) (:secret-key config)]
       (pprint (aws/get-pipeline-definition {:pipeline-id pipeline-id})))))
 
 (defn activate-pipeline
+  "Activate the pipeline with id `pipeline-id`."
   [project [pipeline-id]]
   (let [config (:aws project)]
     (with-credential [(:access-key config) (:secret-key config)]
       (aws/activate-pipeline {:pipeline-id pipeline-id}))))
 
 (defn list-pipelines
+  "Return a list of pipelines."
   [project]
   (let [config (:aws project)]
     (with-credential [(:access-key config) (:secret-key config)]
@@ -24,13 +27,15 @@
 
 (defn create-pipeline
   "Create a new data pipeline with `name` and `unique-id`."
-  [project [name unique-id]]
-  (let [config (:aws project)]
+  [project [name]]
+  (let [config (:aws project)
+        unique-id (.toString (java.util.UUID/randomUUID))]
     (with-credential [(:access-key config) (:secret-key config)]
       (aws/create-pipeline {:name name :unique-id unique-id}))))
 
 (defn put-pipeline
-  "Build a new data pipeline."
+  "Upload a data pipeline definition defined in `path` with
+   the key `pipeline-name`."
   [project [pipeline-name path]]
   (let [config (:aws project)
         pipeline-definitions (parse-flow path)
