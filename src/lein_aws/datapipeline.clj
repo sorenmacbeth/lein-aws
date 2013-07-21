d26374f787b185e9aa5f1425bd1b1611e2428081
Dan Herrera <whoahbot@gmail.com>
2013-07-21 15:34:50 -0700

    Finished initial version of data pipeline.

--- src/lein_aws/datapipeline.clj
+++ src/lein_aws/datapipeline.clj
@@ -1,13 +1,39 @@
 (ns lein-aws.datapipeline
-  (:require [lein-aws.core :refer [parse-flow]]
+  (:require [clojure.pprint :refer [pprint]]
+            [lein-aws.core :refer [parse-flow]]
             [amazonica.core :refer :all]
-            [amazonica.aws.datapipeline :refer :all]))
+            [amazonica.aws.datapipeline :as aws]))
 
-(defn build-pipeline
+(defn get-definition
+  [project [pipeline-id]]
+  (let [config (:aws project)]
+    (with-credential [(:access-key config) (:secret-key config)]
+      (pprint (aws/get-pipeline-definition {:pipeline-id pipeline-id})))))
+
+(defn activate-pipeline
+  [project [pipeline-id]]
+  (let [config (:aws project)]
+    (with-credential [(:access-key config) (:secret-key config)]
+      (aws/activate-pipeline {:pipeline-id pipeline-id}))))
+
+(defn list-pipelines
+  [project]
+  (let [config (:aws project)]
+    (with-credential [(:access-key config) (:secret-key config)]
+      (println (aws/list-pipelines)))))
+
+(defn create-pipeline
+  "Create a new data pipeline with `name` and `unique-id`."
+  [project [name unique-id]]
+  (let [config (:aws project)]
+    (with-credential [(:access-key config) (:secret-key config)]
+      (aws/create-pipeline {:name name :unique-id unique-id}))))
+
+(defn put-pipeline
   "Build a new data pipeline."
   [project [pipeline-name path]]
   (let [config (:aws project)
         pipeline-definitions (parse-flow path)
         pipeline (get pipeline-definitions (keyword pipeline-name))]
     (with-credential [(:access-key config) (:secret-key config)]
-      (put-pipeline-definition pipeline))))
+      (aws/put-pipeline-definition pipeline))))
